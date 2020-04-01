25cfa28e380a74180ac62b08f7c7f82d47b80d0c
ChemIsProgram <adslbbcc1949@gmail.com>
2020-04-01 16:41:32 +1100

    delete files

--- test.py
+++ test.py
@@ -1,25 +1,56 @@
-import asyncio
+import json
 
 
-async def waiter(event):
-    print('waiting for it ...')
-    await event.wait()
-    print('... got it!')
+class MonitorAndDisplay:
 
-async def main():
-    # Create an Event object.
-    event = asyncio.Event()
+    def __init__(self):
+        """Constructer to prepare some initial data from config.json"""
+        self.__r = (255, 0, 0)
+        self.__g = (0, 255, 0)
+        self.__b = (0, 0, 255)
 
-    # Spawn a Task to wait until 'event' is set.
-    waiter_task = asyncio.create_task(waiter(event))
+        try:
+            with open('files/config.json') as file:
+                self.__data = json.load(file)
+            try:
+             self.config_checker(self)
+            except FileNotFoundError as fnfe:
+                print("Something is wrong. " + str(fnfe))
+            except AttributeError as ae:
+                print("Something is wrong. " + str(ae))
+            except ValueError as ve:
+                print("Something is wrong. " + str(ve))
+        except:
+            print("Failed to load config.json.")
 
-    # Sleep for 1 second and set the event.
-    await asyncio.sleep(1)
-    event.set()
+    @staticmethod
+    def config_checker(self):
+        if not self.__data:
+            raise FileNotFoundError("The file is not loaded yet.")
 
-    # Wait until the waiter task is finished.
-    await waiter_task
+        try:
+            if not self.__data['cold_max']:
+                raise AttributeError("The format of the json file is wrong, which is not ""cold_max"".")
+            elif not self.__data['comfortable_min']:
+                raise AttributeError("The format of the json file is wrong, which is not ""comfortable_min"".")
+            elif not self.__data['comfortable_max']:
+                raise AttributeError("The format of the json file is wrong, which is not ""comfortable_max"".")
+            elif not self.__data['hot_min']:
+                raise AttributeError("The format of the json file is wrong, which is not ""hot_min"".")
 
-    print('Done')
+            try:
+                i = int(self.__data['cold_max'])
+                i = int(self.__data['comfortable_min'])
+                i = int(self.__data['comfortable_max'])
+                i = int(self.__data['hot_min'])
+            except ValueError as ve:
+                wrong_input = str(ve)[str(ve).index(":")+1::]
+                raise ValueError("One of the values is not a number, which is :" + wrong_input)
 
-asyncio.run(main())
+        except KeyError as ke:
+            raise AttributeError(
+                "The format of the json file is wrong, which doesn't contain the attribute: " + str(ke))
+
+
+mon = MonitorAndDisplay()
+i = int("25")
